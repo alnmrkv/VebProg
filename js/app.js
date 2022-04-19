d327b9b9ac14f3cc226fece67c23caa887638166
alnmrkv <aln.mrkv@mail.ru>
2022-04-19 22:49:08 +0300

    Работа вкладок. Переработка кода с помощью forEach. Добавление содержимого

--- js/app.js
+++ js/app.js
@@ -1,21 +1,55 @@
 var main = function() {
     "use strict";
-    var makeTabActive  = function (tabNumber){
-        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-    $(".tabs span").removeClass("active");
-    $(tabSelector).addClass("active");
-    };
-    $(".tabs a:nth-child(1)").on("click", function () {
-        makeTabActive(1);
-        return false;
+    var toDos = [
+        "Закончить писать эту книгу",
+        "Вывести Грейси на прогулку в парк",
+        "Ответить на электронные письма",
+        "Подготовиться к лекции в понедельник",
+        "Обновить несколько новых задач",
+        "Купить продукты"
+    ];
+
+    $(".tabs a span").toArray().forEach(function(element) {
+        $(element).on("click", function() {
+
+            var $element = $(element),
+                $content;
+
+            $(".tabs a span").removeClass("active");
+            $element.addClass("active");
+            $("main .content").empty();
+
+            if ($element.parent().is(":nth-child(1)")) {
+
+                $content = $("<ul>");
+                for (var i = toDos.length - 1; i > -1; i--) {
+                    $content.append($("<li>").text(toDos[i]));
+                }
+                $("main .content").append($content)
+
+            } else if ($element.parent().is(":nth-child(2)")) {
+
+                $content = $("<ul>");
+                toDos.forEach(function(todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content)
+
+            } else if ($element.parent().is(":nth-child(3)")) {
+
+                var $input = $("<input>");
+                var $button = $("<button>").text("+")
+                $button.on("click", function() {
+                    toDos.push($input.val());
+                    $input.val("");
+                });
+                $content = $("<div>").append($input).append($button);
+                $("main .content").append($content)
+            }
+            return false;
+        });
     });
-    $(".tabs a:nth-child(2)").on("click", function () {
-        makeTabActive(2);
-        return false;
-    });
-    $(".tabs a:nth-child(3)").on("click", function () {
-        makeTabActive(3);
-        return false;
-    });
-}
+    $(".tabs a:first-child span").trigger("click");
+};
+
 $(document).ready(main);
