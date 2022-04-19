4a2afa2ee53225f2a8f74764b341551352bc46f4
alnmrkv <aln.mrkv@mail.ru>
2022-04-19 22:37:32 +0300

    Работа вкладок

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-var main = function () {
+var main = function() {
     "use strict";
-    console.log("hello, world!");
-    };
-    $(document).ready(main);
+    $(".tabs a:nth-child(1)").on("click" ,function(){
+        $(".tabs span").removeClass("active"); //удаляем активные классы
+        $(".tabs a:nth-child(1) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    })
+    $(".tabs a:nth-child(2)").on("click" ,function(){
+        $(".tabs span").removeClass("active"); //удаляем активные классы
+        $(".tabs a:nth-child(2) span").addClass("active");
+        $("main .content").empty();
+        return false;
+    })
+    $(".tabs a:nth-child(3)").on("click" ,function(){
+        $(".tabs span").removeClass("active"); //удаляем активные классы
+        $(".tabs a:nth-child(3) span").addClass("active");//делаем активной вкладку
+        $("main .content").empty();//очищаем для переопрделения
+        return false;//не переходим по ссылке
+    })
+}
+$(document).ready(main);
