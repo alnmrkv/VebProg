ec900c6dfac765f973ccdfa3abc396414e19d4f1
alnmrkv <aln.mrkv@mail.ru>
2022-04-21 21:53:07 +0300

    Добавлена вкладка теги и скрипт для работы с ним

--- js/app.js
+++ js/app.js
@@ -32,7 +32,23 @@ var main = function(toDoObjects) {
                 $("main .content").append($content)
 
             } else if ($element.parent().is(":nth-child(3)")) {
-                var $input = $("<input>"); 
+                var organizedByTag = tagOrg(toDoObjects);
+
+                organizedByTag.forEach(function(tag) {
+
+                    var $content = $("<ul>");
+                    var $tagName = $("<h3>").text(tag.name);
+
+                    tag.todos.forEach(function(description) {
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    });
+
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                });
+            } else if($element.parent().is(":nth-child(4)")){
+                var $input = $("<input>");
                 var $button = $("<button>").text("+")
                 $button.on("click", function() {
                     toDos.push($input.val());
@@ -45,6 +61,22 @@ var main = function(toDoObjects) {
         });
     });
     $(".tabs a:first-child span").trigger("click");
+};
+
+var tagOrg = function(toDoObjects) {
+    var tagList = [],
+        tagsObject = [];
+    toDoObjects.forEach(function(todo) {
+        todo.tags.forEach(function(tag) {
+            if (tagList.includes(tag)) {
+                tagsObject[tagList.indexOf(tag)].todos.push(todo.description);
+            } else {
+                tagList.push(tag);
+                tagsObject.push({ "name": tag, "todos": [todo.description] });
+            }
+        });
+    });
+    return tagsObject;
 };
 
 $(document).ready(function(){
